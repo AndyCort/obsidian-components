274dce126874b5e24247c05c1aa83ecbf0d16cee
AndyCort <1771858268@qq.com>
2026-02-23 12:01:46 +0800

    fix: color component handles pasted CSS with property prefixes

--- examples/color.md
+++ examples/color.md
@@ -6,7 +6,7 @@ props:
   name: ""
 ---
 
-<span class="oc-color-dot" style="background: {{color}};" title="{{color}}"></span>{{#if name}}<span class="oc-color-label">{{name}}</span>{{/if}}
+<span class="oc-color-dot"></span>{{#if name}}<span class="oc-color-label">{{name}}</span>{{/if}}
 
 <style>
 .oc-color-dot {
@@ -25,3 +25,12 @@ props:
   vertical-align: middle;
 }
 </style>
+
+<script>
+var dot = el.querySelector('.oc-color-dot');
+if (dot) {
+  var c = (props.color || '#6366f1').replace(/^(background(-image)?)\s*:\s*/i, '').replace(/;\s*$/, '');
+  dot.style.background = c;
+  dot.title = c;
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -188,12 +188,21 @@ props:
   name: ""
 ---
 
-<span class="oc-color-dot" style="background: {{color}};" title="{{color}}"></span>{{#if name}}<span class="oc-color-label">{{name}}</span>{{/if}}
+<span class="oc-color-dot"></span>{{#if name}}<span class="oc-color-label">{{name}}</span>{{/if}}
 
 <style>
 .oc-color-dot { display: inline-block; width: 1em; height: 1em; border-radius: 50%; vertical-align: middle; box-shadow: 0 0 0 1.5px rgba(0,0,0,0.1), 0 1px 3px rgba(0,0,0,0.1); flex-shrink: 0; }
 .oc-color-label { font-size: 0.85em; color: var(--text-muted); margin-left: 3px; vertical-align: middle; }
-</style>`,
+</style>
+
+<script>
+var dot = el.querySelector('.oc-color-dot');
+if (dot) {
+  var c = (props.color || '#6366f1').replace(/^(background(-image)?)\\s*:\\s*/i, '').replace(/;\\s*$/, '');
+  dot.style.background = c;
+  dot.title = c;
+}
+</script>`,
 };
 
 export default class ComponentsPlugin extends Plugin {
